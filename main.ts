525ad3348d21236c01a73b470b6e03e8e5d0737d
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-10-02 13:04:30 -0400

    Final commit

--- main.ts
+++ main.ts
@@ -3,6 +3,14 @@
 // 
 // This program detects how far something is
 // 
+let distance = 0
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
-	
+    distance = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showString("" + (distance))
+    basic.pause(500)
 })
