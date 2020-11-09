6da4e75910c88cc7e0146f49f6decec88207d40d
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-11-09 14:07:11 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,46 +1,255 @@
-// Created by Vaughn Hatherall
-// Created on October 1 2020
-// 
-// This program detects how far something is
-// 
-let strip: neopixel.Strip = null
 let distance = 0
-basic.showIcon(IconNames.Heart)
+let strip: neopixel.Strip = null
+input.onButtonPressed(Button.A, function () {
+    robotbit.MotorRun(robotbit.Motors.M1A, -132)
+})
+basic.forever(function () {
+    if (distance > 24) {
+        robotbit.MotorRun(robotbit.Motors.M2B, 78)
+        basic.pause(1000)
+        robotbit.MotorStop(robotbit.Motors.M2B)
+    }
+})
+basic.forever(function () {
+	
+})
 basic.forever(function () {
     distance = sonar.ping(
     DigitalPin.P1,
     DigitalPin.P2,
-    PingUnit.Centimeters
+    PingUnit.Inches
     )
     basic.showString("" + (distance))
-    basic.pause(500)
-    if (distance < 2) {
-        strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        strip.showColor(neopixel.colors(NeoPixelColors.Red))
-    }
-    if (distance > 5) {
-        strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        strip.showColor(neopixel.colors(NeoPixelColors.Green))
-    }
-    if (distance == 2) {
-        strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
-        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
-        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
-        strip.show()
-    }
-    if (distance == 3) {
-        strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    basic.pause(200)
+    if (distance > 2) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
         strip.show()
     }
-    if (distance == 4) {
-        strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+    if (distance > 4) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
         strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
         strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
         strip.show()
     }
+    if (distance > 6) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 8) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 10) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 12) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 14) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 16) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 18) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 20) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(19, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(20, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 22) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(19, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(20, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(21, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(22, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+    if (distance > 24) {
+        strip = neopixel.create(DigitalPin.P15, 24, NeoPixelMode.RGB)
+        strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(5, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(6, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(7, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(8, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(9, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(10, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(11, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(12, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(13, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(14, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(15, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(16, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(17, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(18, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(19, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(20, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(21, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(22, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(23, neopixel.colors(NeoPixelColors.Blue))
+        strip.setPixelColor(24, neopixel.colors(NeoPixelColors.Blue))
+        strip.show()
+    }
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
 })
